1a177dcfa652fc7d5a9dbcceb81b66819454c8cb
Evan Nichols <33175950+Reccho@users.noreply.github.com>
2021-12-14 11:41:08 +0100

    Problem 2

--- 1to28_Lists.hs
+++ 1to28_Lists.hs
@@ -1,7 +1,16 @@
--- P01 (*): Find the last element of a list.
+-- P01 (*) Find the last element of a list.
 myLast :: [a] -> a
 myLast [] = error "Empty list -> no end in sight."
 myLast [x] = x
 myLast (_:xs) = myLast xs
 
 myLast' = head . reverse
+
+
+-- P02 (*) Find the last but one element of a list.
+myButLast :: [a] -> a
+myButLast [] = error "Empty list -> no 2nd-to-last in sight"
+myButLast [x, _] = x
+myButLast (x:xs) = myButLast xs
+
+myButLast' = last . init
